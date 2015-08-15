f30366dcdc98fa6390c328753b3b807707f8da2c
ashrafeme <ashraf_eme@hotmail.com>
2015-08-15 23:12:18 +0300

    TestSet21

---
+++ MUMSPT/TestSet21/TestSet21.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUMSPT.TestSet21
+{
+    public static class TestSet21
+    {
+        /// <summary>
+        /// An array is called systematically increasing if it 
+        /// consists of increasing sequences of the numbers from 1 to n.
+        /// The first six (there are over 65,000 of them) systematically increasing arrays are:
+        /// {1}
+        /// {1, 1, 2}
+        /// {1, 1, 2, 1, 2, 3}
+        /// {1, 1, 2, 1, 2, 3, 1, 2, 3, 4}
+        /// {1, 1, 2, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5}
+        /// {1, 1, 2, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6}
+        /// Write a function named isSystematicallyIncreasing which 
+        /// returns 1 if its array argument is systematically increasing. Otherwise it returns 0.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static int isSystematicallyIncreasing(int[] a)
+        {
+            int isSystematicallyIncreasing = 0;
+
+            int i = 0, k = 0;
+            int current = 0;
+            do
+            {
+                current = a[i];
+                k = i;
+                do
+                {
+                    //if()
+                    k++;
+                } while (k <= i + i);
+
+                i = k;
+
+            } while (i< a.Length);
+
+            return isSystematicallyIncreasing;
+        }
+    }
+}
---
+++ MUMSPT/TestSet21/TestSet21Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUMSPT.TestSet21
+{
+  public static class TestSet21Test
+    {
+    }
+}
